77af2e0c68d47063e6c37bec75abbf8f636b5115
Eiyeron Fulmincendii <eiyeron@gmail.com>
2015-06-20 14:04:04 +0200

    Made version untracked for cleaning a bit the mess

--- include/version.h
+++
@@ -1,4 +0,0 @@
-#ifndef INCLUDE_VERSION_H
-#define INCLUDE_VERSION_H
-extern const char git_version[];
-#endif
--- src/version.c
+++
@@ -1,3 +0,0 @@
-#include "version.h"
-
-const char git_version[] = "renderer-89b5e0b";
