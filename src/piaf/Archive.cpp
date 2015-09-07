860b5bb5ada754b2f0f14b2e8b3daa48a81a2965
Eiyeron Fulmincendii <eiyeron@gmail.com>
2015-09-07 23:41:48 +0200

    Forgot to format

--- src/piaf/Archive.cpp
+++ src/piaf/Archive.cpp
@@ -20,15 +20,17 @@ Archive::~Archive()
 
 File_Entry Archive::get_file_entry(char *filename)
 {
-    for (uint32_t index = 0; index < nb_files; index++) {
-        if(strcmp(entries[index].filename, filename) == 0) {
+    for (uint32_t index = 0; index < nb_files; index++)
+    {
+        if (strcmp(entries[index].filename, filename) == 0)
+        {
             return entries[index];
         }
     }
     // throw exception
 }
 
-/*PFile*/ void /*File_Entry*/get(char*filename)
+/*PFile*/ void /*File_Entry*/ get(char *filename)
 {
     // return PFile(get_file_entry(filename));
 }
