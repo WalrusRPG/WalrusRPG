4072a8cad4b0b214c7fcac3ee12b455ae1cdd88f
Eiyeron Fulmincendii <eiyeron@gmail.com>
2016-01-23 12:24:52 +0100

    Fixed a stupid contructor error.

--- src/utility/Rect.h
+++ src/utility/Rect.h
@@ -10,9 +10,9 @@ namespace WalrusRPG
             signed x, y;
             unsigned width, height;
             // We don't need a source file for two inline functions.
-            Rect(unsigned x_, unsigned y_, signed width_, signed height_)
+            Rect(signed x_, signed y_, unsigned width_, unsigned height_)
                 : x(x_), y(y_), width(width_), height(height_){};
-            Rect(unsigned x_, unsigned y_) : Rect(x_, y_, 0, 0){};
+            Rect(signed x_, signed y_) : Rect(x_, y_, 0, 0){};
             Rect() : Rect(0, 0, 0, 0){};
         };
     }
