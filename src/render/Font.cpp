c0c39453578d304d8aa0bc816d89444e5e0e820e
Eiyeron Fulmincendii <eiyeron@gmail.com>
2016-02-09 16:30:22 +0100

    Updated a bit the parser reader.

--- src/render/Font.cpp
+++ src/render/Font.cpp
@@ -26,12 +26,12 @@ Font::Font(Texture& font_tex, WalrusRPG::PIAF::File font_config)
 	for (int i = 0; i < 256; ++i)
 	{
 		const uint8_t* current_char = ptr + 9 + (6*4+2)*i;
-		chars[i].dimensions.x = read_big_endian_value<signed>(current_char);
-		chars[i].dimensions.y = read_big_endian_value<signed>(current_char+4);
-		chars[i].dimensions.width = read_big_endian_value<unsigned>(current_char+8);
-		chars[i].dimensions.height = read_big_endian_value<unsigned>(current_char+12);
-		chars[i].x_offset = read_big_endian_value<unsigned>(current_char+16);
-		chars[i].y_offset = read_big_endian_value<unsigned>(current_char+18);
+		chars[i].dimensions.x = read_big_endian_value<int16_t>(current_char);
+		chars[i].dimensions.y = read_big_endian_value<int16_t>(current_char+4);
+		chars[i].dimensions.width = read_big_endian_value<uint16_t>(current_char+8);
+		chars[i].dimensions.height = read_big_endian_value<uint16_t>(current_char+12);
+		chars[i].x_offset = read_big_endian_value<uint16_t>(current_char+16);
+		chars[i].y_offset = read_big_endian_value<uint16_t>(current_char+18);
 	}
 }
 
